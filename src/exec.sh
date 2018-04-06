ba4034204fcd563229dc3241927482f82fc735de
Kirill Chernyshov <delaguardo@gmail.com>
2018-04-06 10:53:50 +0200

    Regenerate compose files before exec, log and restart command

---
+++ src/exec.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+$YODA_BIN compose > /dev/null
+docker-compose -f $MAIN_COMPOSE_FILE -f $COMPOSE_FILE exec $@
--- src/restart.sh
+++ src/restart.sh
@@ -4,4 +4,5 @@ set -e
 source $YODA_PATH/lib/container.sh
 containers=$(get_containers "$@")
 
+$YODA_BIN compose > /dev/null
 docker-compose -f $MAIN_COMPOSE_FILE -f $COMPOSE_FILE restart -t $STOP_WAIT_TIMEOUT ${containers[*]}
