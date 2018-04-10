e0bd6821f5e91988225fecd1f15ab9d1f4fefdc1
Kirill Chernyshov <delaguardo@gmail.com>
2018-04-10 10:34:41 +0200

    Remove echo from exec command, add ability to pass arguments into kompose

--- src/exec.sh
+++ src/exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-echo docker-compose $COMPOSE_FILES_ARGS exec $@
+docker-compose $COMPOSE_FILES_ARGS exec $@
--- src/kompose.sh
+++ src/kompose.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+for p in "$@"; do
+  case $p in
+    --env=*)
+      shift
+      ;;
+  esac
+done
+
 command -v kompose >/dev/null 2>&1 || { echo >&2 "kompose is not installed. Please check this link for installation instructions - http://kompose.io/"; exit 1; }
-kompose convert $COMPOSE_FILES_ARGS --stdout
+kompose convert $COMPOSE_FILES_ARGS $@
