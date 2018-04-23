985d3459966adc371a6d3302006272403dc80506
Kirill Chernyshov <delaguardo@gmail.com>
2018-04-23 09:48:25 +0200

    Fix kompose using

--- src/kompose.sh
+++ src/kompose.sh
@@ -2,10 +2,12 @@
 set -e
 
 command="convert"
+args=""
 
 for p in "$@"; do
   case $p in
     --env=*)
+      args+=" $p"
       shift
       ;;
     convert|down|up)
@@ -16,4 +18,6 @@ for p in "$@"; do
 done
 
 command -v kompose >/dev/null 2>&1 || { echo >&2 "kompose is not installed. Please check this link for installation instructions - http://kompose.io/"; exit 1; }
-kompose $command $COMPOSE_FILES_ARGS $@
+docker-compose $COMPOSE_FILES_ARGS config | sed "s/^    hostname:.*$//g" > /tmp/docker-compose.yml
+kompose -f /tmp/docker-compose.yml $command
+rm /tmp/docker-compose.yml
